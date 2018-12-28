a48eda661ab2eca33e04013d089c9bf6eb70f6c1
Christof Laenzlinger <christof.laenzlinger@swisscom.com>
2018-12-28 23:54:17 +0100

    Read a button status and send note on/off commands on interrupt.

--- midibox.go
+++ midibox.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net"
 	"time"
+
+	"github.com/warthog618/gpio"
 )
 
 func main() {
@@ -11,24 +13,47 @@ func main() {
 	conn, err := net.Dial("udp", "127.0.0.1:5006")
 	defer conn.Close()
 	if err != nil {
-		fmt.Printf("Some error %v", err)
-		return
+		panic(err)
 	}
 
-	noteOn(conn)
+	handler := func(pin *gpio.Pin) {
+		if pin.Read() {
+			noteOff(conn)
+		} else {
+			noteOn(conn)
+		}
+	}
 
-	time.Sleep(1 * time.Second)
+	gpioErr := gpio.Open()
+	defer gpio.Close()
+	if gpioErr != nil {
+		panic(gpioErr)
+	}
 
-	noteOff(conn)
+	pin := gpio.NewPin(gpio.GPIO5)
+	pin.Input()
+	pin.PullUp()
+	watchErr := pin.Watch(gpio.EdgeBoth, handler)
+	defer pin.Unwatch()
+	if watchErr != nil {
+		panic(watchErr)
+	}
+
+	fmt.Println("setup done")
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Second)
+	}
 
 }
 
 func noteOn(conn net.Conn) {
+	fmt.Println("note on")
 	var noteOn = []byte{0xaa, 0x96, 0x3c, 0x7f}
 	conn.Write(noteOn)
 }
 
 func noteOff(conn net.Conn) {
+	fmt.Println("note off")
 	var noteOff = []byte{0xaa, 0x86, 0x3c, 0x7f}
 	conn.Write(noteOff)
 }
