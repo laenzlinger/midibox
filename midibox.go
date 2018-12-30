38c169695007da27ae9e50634ac04ecac0b7d994
Christof Laenzlinger <christof.laenzlinger@swisscom.com>
2018-12-30 15:33:01 +0100

    introduce upDown scanner

--- midibox.go
+++ midibox.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"log"
+	"net"
 	"time"
 
 	"periph.io/x/periph/conn/gpio"
-    "periph.io/x/periph/conn/gpio/gpioreg"
+	"periph.io/x/periph/conn/gpio/gpioreg"
 	"periph.io/x/periph/host"
 )
 
-// UpDown is sent when one of the up/down buttons is pressed. 
+// UpDown is sent when one of the up/down buttons is pressed.
 //  -------------------
 //           |        |
 //   OLED    |     x  | up button
@@ -39,12 +39,11 @@ func (upDown UpDown) PinName() string {
 	if upDown {
 		return "GPIO6"
 	}
-	return "GPIO6"
+	return "GPIO5"
 }
 
-
 // JoystickDirection represents the position of the joystick
-//        North      
+//        North
 //       --------
 // West  | None | East
 //       --------
@@ -53,11 +52,11 @@ type JoystickDirection uint8
 
 const (
 	// None not moved
-	None  JoystickDirection = iota 
+	None JoystickDirection = iota
 	// North direction movement
 	North
 	// East direction movement
-	East 
+	East
 	// South direction movement
 	South
 	// West direction movement
@@ -65,65 +64,91 @@ const (
 )
 
 func (dir JoystickDirection) String() string {
-    names := [...]string{
-        "None", 
-        "North", 
-        "East", 
-        "South",
-		"West", 
+	names := [...]string{
+		"None",
+		"North",
+		"East",
+		"South",
+		"West",
 	}
-    if dir < None || dir > West {
-      return "Unknown"
-    }
-    return names[dir]
+	if dir < None || dir > West {
+		return "Unknown"
+	}
+	return names[dir]
 }
 
 // PinName returns the name of the GPIO pin
 func (dir JoystickDirection) PinName() string {
-    pinNames := [...]string{
-        "GPIO4", 
-        "GPIO17", 
-        "GPIO23", 
-        "GPIO22",
-		"GPIO27", 
+	pinNames := [...]string{
+		"GPIO4",
+		"GPIO17",
+		"GPIO23",
+		"GPIO22",
+		"GPIO27",
 	}
-    return pinNames[dir]
+	return pinNames[dir]
 }
 
 // Joystick is sent when a joystick action is detected.
 type Joystick struct {
 	Direction JoystickDirection
-	Active bool
+	Active    bool
 }
 
-func watchUpDown(upDown chan <- UpDown, value UpDown) {
+func watchUpDown(upDown chan<- UpDown, keyboardTicker *time.Ticker) {
+
+	up := gpioreg.ByName(Up.PinName())
+	down := gpioreg.ByName(Down.PinName())
+
+	if err := up.In(gpio.PullUp, gpio.NoEdge); err != nil {
+		log.Fatal(err)
+	}
+	if err := down.In(gpio.PullUp, gpio.NoEdge); err != nil {
+		log.Fatal(err)
+	}
+
+	var active = false
+	var changed = time.Now()
+	for tickTime := range keyboardTicker.C {
+		var value UpDown
+		var buttonPressed = false
+		if up.Read() == gpio.Low {
+			buttonPressed = true
+			value = Up
+		} else if down.Read() == gpio.Low {
+			buttonPressed = true
+			value = Down
+		}
+		if buttonPressed {
+			if active {
+				if time.Since(changed) > 500 * time.Millisecond {
+					upDown <- value
+				}		
+			} else {
+				active = true
+				changed = tickTime
+				upDown <- value
+			}
+		} else {
+			active = false
+			changed = time.Now()
+		}
+
+	}
+}
+
+func watchJoystick(joystick chan<- Joystick, value JoystickDirection) {
 	p := gpioreg.ByName(value.PinName())
 
 	if err := p.In(gpio.PullUp, gpio.FallingEdge); err != nil {
 		log.Fatal(err)
 	}
-	
-	for {
-		p.WaitForEdge(-1)
-		upDown <- value
-		p.In(gpio.PullNoChange, gpio.NoEdge)
-		time.Sleep(250 * time.Millisecond)
-		p.In(gpio.PullNoChange, gpio.FallingEdge)
-	}
-}
 
-func watchJoystick(joystick chan <- Joystick, value JoystickDirection) {
-	p := gpioreg.ByName(value.PinName())
-
-	if err := p.In(gpio.PullUp, gpio.FallingEdge); err != nil {
-		log.Fatal(err)
-	}
-	
 	for {
 		p.WaitForEdge(-1)
 		joystick <- Joystick{
 			Direction: value,
-			Active: false,
+			Active:    false,
 		}
 		p.In(gpio.PullNoChange, gpio.NoEdge)
 		time.Sleep(250 * time.Millisecond)
@@ -143,25 +168,25 @@ func main() {
 		log.Fatal(err)
 	}
 
+	keyboardTicker := time.NewTicker(50 * time.Millisecond)
 	upDown := make(chan UpDown)
-	go watchUpDown(upDown, Down)
-	go watchUpDown(upDown, Up)
+	go watchUpDown(upDown, keyboardTicker)
 
 	joystick := make(chan Joystick)
-	go watchJoystick(joystick, None)
-	go watchJoystick(joystick, North)
-	go watchJoystick(joystick, East)
-	go watchJoystick(joystick, South)
-	go watchJoystick(joystick, West)
+	// go watchJoystick(joystick, None)
+	// go watchJoystick(joystick, North)
+	// go watchJoystick(joystick, East)
+	// go watchJoystick(joystick, South)
+	// go watchJoystick(joystick, West)
 
 	for i := 0; i < 10; i++ {
-        select {
+		select {
 		case upDown := <-upDown:
 			fmt.Println("upDown:", upDown)
 		case joystick := <-joystick:
 			fmt.Println("joystick:", joystick)
-        }
-    }
+		}
+	}
 
 }
 
