a9bb90061e5fc520882afaced09817b0718022b5
Christof Laenzlinger <christof.laenzlinger@swisscom.com>
2019-01-02 04:04:15 +0100

    extract the differnt modes and use a mode interface

--- midibox.go
+++ midibox.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"github.com/laenzlinger/midibox/mode"
 	"log"
 
 	"github.com/laenzlinger/midibox/display"
 	"github.com/laenzlinger/midibox/keyboard"
-	"github.com/laenzlinger/midibox/midi"
-	"github.com/laenzlinger/midibox/preset"
 
 	"periph.io/x/periph/host"
 )
@@ -20,49 +19,21 @@ func main() {
 	display := display.Open()
 	defer display.Clear()
 
-	md := midi.Open()
-	defer md.Close()
-
 	upDown := keyboard.OpenUpDown()
 	joystick := keyboard.OpenJoystick()
 
-	presets := preset.AllPresets()
-	var active bool
-
-
-	display.DrawText("Select Preset", presets.Current().Name())
+	
+    active := mode.InitialMode(display)
 
 	for i := 0; i < 50; i++ {
 		select {
 		case u := <-upDown:
-			if active {
-				if u == keyboard.Both {
-					active = false
-					display.DrawText("Select Preset", presets.Current().Name())
-					presets.Current().Shutdown()
-				} else {
-					presets.Current().OnUpDwon(u)
-				}
-			}
+			active = active.OnUpDwon(u)
 		case j := <-joystick:
-			if active {
-				presets.Current().OnJoystick(j)
-			} else {
-				if j.Direction == keyboard.North {
-					presets.Previous()
-					display.DrawText("Select Preset", presets.Current().Name())
-				} else if j.Direction == keyboard.South {
-					presets.Next()
-					display.DrawText("Select Preset", presets.Current().Name())
-				} else if j.Fire && j.FireChanged {
-					display.DrawText("Active Preset", presets.Current().Name())
-					presets.Current().Init(md)
-					active = true
-				}
-			}
+			active = active.OnJoystick(j)
 		}
 	}
 
-	presets.Current().Shutdown()
+	active.Exit()
 
 }
--- mode/mode.go
+++ mode/mode.go
@@ -1,18 +1,95 @@
 package mode
 
 import (
+	"github.com/laenzlinger/midibox/display"
 	"github.com/laenzlinger/midibox/keyboard"
+	"github.com/laenzlinger/midibox/midi"
+	"github.com/laenzlinger/midibox/preset"
 )
 
 // Mode represents the current mode of operation of the midibox
 type Mode interface {
-	// Enter the mode
-	Enter() *Mode
 	// OnJoystick reacts on Joystick input
-	OnJoystick(j keyboard.Joystick) *Mode
+	OnJoystick(j keyboard.Joystick) Mode
 	// OnUpDown reacts on UpDown input
-	OnUpDwon(j keyboard.UpDown) *Mode
+	OnUpDwon(u keyboard.UpDown) Mode
 	// Exit the mode
-	Exit() *Mode
+	Exit() Mode
 }
 
+// InitialMode returns the inital mode.
+func InitialMode(display display.Display) Mode {
+	m := selectPreset{}
+	return m.Enter(display)
+}
+
+// SelectPreset is the mode which allows to select the preset.
+type selectPreset struct {
+	display display.Display
+	presets preset.Presets
+}
+
+func (m *selectPreset) Enter(display display.Display) Mode {
+	m.presets = preset.AllPresets()
+	m.display = display
+	m.display.DrawText("Select Preset", m.presets.Current().Name())
+	return m
+}
+
+func (m *selectPreset) OnJoystick(j keyboard.Joystick) Mode {
+	if j.Fire && j.FireChanged {
+		next := playMode{}
+		return next.Enter(m.display, m.presets.Current()) 
+	}
+	if j.Direction == keyboard.North {
+		m.presets.Previous()
+	} else if j.Direction == keyboard.South {
+		m.presets.Next()
+	}  
+	m.display.DrawText("Select Preset", m.presets.Current().Name())
+	return m
+}
+
+func (m *selectPreset) OnUpDwon(u keyboard.UpDown) Mode {
+	return m
+}
+
+func (m *selectPreset) Exit() Mode {
+	return m
+}
+
+// playMode is the mode where the selected preset is played
+type playMode struct {
+	display display.Display
+	preset  preset.Preset
+	driver  midi.Driver
+}
+
+func (m *playMode) Enter(display display.Display, preset preset.Preset) Mode {
+	m.preset = preset
+	m.display = display
+	m.driver = midi.Open()
+	m.display.DrawText("Active Preset", m.preset.Name())
+	m.preset.Init(m.driver)
+	return m
+}
+
+func (m *playMode) OnJoystick(j keyboard.Joystick) Mode {
+	m.preset.OnJoystick(j)
+	return m
+}
+
+func (m *playMode) OnUpDwon(u keyboard.UpDown) Mode {
+	if u == keyboard.Both {		
+		return m.Exit()
+	}
+	m.preset.OnUpDwon(u)
+	return m
+}
+
+func (m *playMode) Exit() Mode {
+	m.preset.Shutdown()
+	m.driver.Close()
+	next := selectPreset{}
+	return next.Enter(m.display)
+}
